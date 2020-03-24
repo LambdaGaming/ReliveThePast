e67c061f07e135eb2e32f43c9d16812697ae57db
Hashim <42122270+DefyTheRush@users.noreply.github.com>
2020-03-24 00:26:23 -0400

    Add files for ReliveThePast v1
    Added:
    - Toggle to enable or disable plugin
    - Basic mechanics: Respawns any player after they die

---
+++ ReliveThePast/EventHandlers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using EXILED;
+using EXILED.Extensions;
+using MEC;
+
+namespace ReliveThePast
+{
+    public class EventHandlers
+    {
+        public void RunOnPlayerDeath(ref PlayerDeathEvent d)
+        {
+            ReferenceHub hub = d.Player;
+            Timing.CallDelayed(0.05f, () => RevivePlayer(hub));
+        }
+
+        public void RevivePlayer(ReferenceHub rh)
+        {
+            Random randNum = new Random();
+            int num = randNum.Next(0, 7);
+
+            switch (num)
+            {
+                case 0:
+                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCadet, rh.gameObject);
+                    break;
+                case 1:
+                    rh.characterClassManager.SetPlayersClass(RoleType.ClassD, rh.gameObject);
+                    break;
+                case 2:
+                    rh.characterClassManager.SetPlayersClass(RoleType.FacilityGuard, rh.gameObject);
+                    break;
+                case 3:
+                    rh.characterClassManager.SetPlayersClass(RoleType.NtfLieutenant, rh.gameObject);
+                    break;
+                case 4:
+                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCadet, rh.gameObject);
+                    break;
+                case 5:
+                    rh.characterClassManager.SetPlayersClass(RoleType.ChaosInsurgency, rh.gameObject);
+                    break;
+                case 6:
+                    rh.characterClassManager.SetPlayersClass(RoleType.Scientist, rh.gameObject);
+                    break;
+                case 7:
+                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCommander, rh.gameObject);
+                    break;
+            }
+        }
+    }
+}
---
+++ ReliveThePast/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EXILED;
+
+namespace ReliveThePast
+{
+    public class Plugin : EXILED.Plugin
+    {
+        public bool EnableRelive;
+        public override string getName { get; } = "Re-live The Past - By DefyTheRush";
+
+        public EventHandlers Handler;
+
+        public void ReloadConfig()
+        {
+            EnableRelive = Config.GetBool("enable_relive", true);
+            if (!EnableRelive)
+                Log.Info("Plugin disabled!");
+            else
+                Log.Info("Plugin enabled!");
+        }
+        public override void OnDisable()
+        {
+            Events.PlayerDeathEvent -= Handler.RunOnPlayerDeath;
+            Handler = null;
+        }
+
+        public override void OnEnable()
+        {
+            ReloadConfig();
+            if (!EnableRelive)
+                return;
+
+            Info("Starting up \"Re-live The Past - By DefyTheRush\"");
+            Handler = new EventHandlers();
+            Events.PlayerDeathEvent += Handler.RunOnPlayerDeath;
+        }
+
+        public override void OnReload()
+        {
+            Info("Reloading \"Re-live The Past\"!");
+        }
+    }
+}

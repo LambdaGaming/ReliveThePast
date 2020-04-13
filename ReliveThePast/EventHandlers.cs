8463210e9eb8ba73bfc828eee9ae2e7d6fb6dc81
LambdaGaming <starwars18m@gmail.com>
2020-04-12 23:54:24 -0400

    Players will now only spawn as scientist or class D
    Also fixed players spawning as a different role after they respawn through vanilla means such as an MTF or Chaos wave.

--- ReliveThePast/EventHandlers.cs
+++ ReliveThePast/EventHandlers.cs
@@ -1,129 +1,82 @@
 using System;
 using EXILED;
 using EXILED.Extensions;
-using EXILED.Patches;
 using MEC;
 
 namespace ReliveThePast
 {
-    public class EventHandlers
-    {
-        Random randNum = new Random();
-        float RespawnTimerValue = (float) Plugin.ReliveRespawnTimer;
-        bool IsWarheadDetonated;
-        bool IsDecontanimationActivated;
-        bool AllowRespawning = false;
+	public class EventHandlers
+	{
+		Random randNum = new Random();
+		float RespawnTimerValue = ( float ) Plugin.ReliveRespawnTimer;
+		bool IsWarheadDetonated;
+		bool IsDecontanimationActivated;
+		bool AllowRespawning = true;
 
-        public void RunOnPlayerDeath(ref PlayerDeathEvent d)
-        {
-            ReferenceHub hub = d.Player;
-            IsWarheadDetonated = Map.IsNukeDetonated;
-            IsDecontanimationActivated = Map.IsLCZDecontaminated;
-            if (AllowRespawning == true)
-                Timing.CallDelayed(RespawnTimerValue, () => RevivePlayer(hub));
-        }
+		public void RunOnPlayerDeath( ref PlayerDeathEvent d )
+		{
+			ReferenceHub hub = d.Player;
+			IsWarheadDetonated = Map.IsNukeDetonated;
+			IsDecontanimationActivated = Map.IsLCZDecontaminated;
+			if ( IsWarheadDetonated || IsDecontanimationActivated )
+				AllowRespawning = false;
+			if ( AllowRespawning )
+				Timing.CallDelayed( RespawnTimerValue, () => RevivePlayer( hub ) );
+		}
 
-        public void RunOnRoundRestart()
-        {
-            AllowRespawning = false;
-        }
+		public void RunOnRoundRestart()
+		{
+			AllowRespawning = true;
+		}
 
-        public void RunOnCommand(ref RACommandEvent r)
-        {
-            try
-            {
-                string arg = r.Command;
-                ReferenceHub sender = r.Sender.SenderId == "SERVER CONSOLE" || r.Sender.SenderId == "GAME CONSOLE" ? PlayerManager.localPlayer.GetPlayer() : Player.GetPlayer(r.Sender.SenderId);
+		public void RunOnCommand( ref RACommandEvent r )
+		{
+			try
+			{
+				string arg = r.Command;
+				ReferenceHub sender = r.Sender.SenderId == "SERVER CONSOLE" || r.Sender.SenderId == "GAME CONSOLE" ? PlayerManager.localPlayer.GetPlayer() : Player.GetPlayer( r.Sender.SenderId );
 
-                switch (arg.ToLower())
-                {
-                    case "allowautorespawn":
-                        r.Allow = false;
-                        if (!sender.CheckPermission("rtp.allow"))
-                        {
-                            r.Sender.RAMessage("You are not authorized to use this command");
-                            return;
-                        }
-                        if (AllowRespawning == false)
-                        {
-                            r.Sender.RAMessage("Auto respawning enabled!");
-                            Map.Broadcast("<color=green>Auto respawning enabled!</color>", 5);
-                            AllowRespawning = true;
-                        }
-                        else
-                        { 
-                            r.Sender.RAMessage("Auto respawning disabled!");
-                            Map.Broadcast("<color=red>Auto respawning disabled!</color>", 5);
-                            AllowRespawning = false;
-                        }
-                        break;
-                }
-            }
-            catch (Exception)
-            {
-                Log.Info("There was an error handling this command");
-            }
-        }
+				if ( arg.ToLower() == "allowautorespawn" )
+				{
+					r.Allow = false;
+					if ( !sender.CheckPermission( "rtp.allow" ) )
+					{
+						r.Sender.RAMessage( "You are not authorized to use this command" );
+						return;
+					}
+					if ( !AllowRespawning )
+					{
+						r.Sender.RAMessage( "Auto respawning enabled!" );
+						Map.Broadcast( "<color=green>Auto respawning enabled!</color>", 5 );
+						AllowRespawning = true;
+					}
+					else
+					{
+						r.Sender.RAMessage( "Auto respawning disabled!" );
+						Map.Broadcast( "<color=red>Auto respawning disabled!</color>", 5 );
+						AllowRespawning = false;
+					}
+				}
+			}
+			catch ( Exception )
+			{
+				Log.Info( "There was an error handling this command" );
+			}
+		}
 
-        public void RevivePlayer(ReferenceHub rh)
-        {
-            int num = randNum.Next(0, 7);
-
-            switch (num)
-            {
-                case 0:
-                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCadet, rh.gameObject);
-                    break;
-                case 1:  
-                    if (!IsWarheadDetonated && !IsDecontanimationActivated)
-                    {
-                        rh.characterClassManager.SetPlayersClass(RoleType.ClassD, rh.gameObject);
-                        return;
-                    }
-                    if (!IsWarheadDetonated && IsDecontanimationActivated)
-                    {
-                        rh.characterClassManager.SetPlayersClass(RoleType.ChaosInsurgency, rh.gameObject);
-                        return;
-                    }
-                    if (IsWarheadDetonated || IsDecontanimationActivated)
-                        rh.characterClassManager.SetPlayersClass(RoleType.ChaosInsurgency, rh.gameObject);
-                    break;
-                case 2:
-                    if (!IsWarheadDetonated)
-                    {
-                        rh.characterClassManager.SetPlayersClass(RoleType.FacilityGuard, rh.gameObject);
-                        return;
-                    }
-                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCommander, rh.gameObject);
-                    break;
-                case 3:
-                    rh.characterClassManager.SetPlayersClass(RoleType.NtfLieutenant, rh.gameObject);
-                    break;
-                case 4:
-                    rh.characterClassManager.SetPlayersClass(RoleType.NtfScientist, rh.gameObject);
-                    break;
-                case 5:
-                    rh.characterClassManager.SetPlayersClass(RoleType.ChaosInsurgency, rh.gameObject);
-                    break;
-                case 6:
-                    if (!IsWarheadDetonated && !IsDecontanimationActivated)
-                    {
-                        rh.characterClassManager.SetPlayersClass(RoleType.Scientist, rh.gameObject);
-                        return;
-                    }
-                    if (!IsWarheadDetonated && IsDecontanimationActivated)
-                    {
-                        rh.characterClassManager.SetPlayersClass(RoleType.NtfLieutenant, rh.gameObject);
-                        return;
-                    }
-                    if (IsWarheadDetonated || IsDecontanimationActivated)
-                        rh.characterClassManager.SetPlayersClass(RoleType.NtfLieutenant, rh.gameObject);
-                    break;
-                case 7:
-                    rh.characterClassManager.SetPlayersClass(RoleType.NtfCommander, rh.gameObject);
-                    break;
-            }
-        }
-    }
+		public void RevivePlayer( ReferenceHub rh )
+		{
+			if ( rh.GetRole() != RoleType.Spectator ) return;
+			int num = randNum.Next( 0, 1 );
+			switch ( num )
+			{
+				case 0:
+					rh.characterClassManager.SetPlayersClass( RoleType.Scientist, rh.gameObject );
+					break;
+				case 1:
+					rh.characterClassManager.SetPlayersClass( RoleType.ClassD, rh.gameObject );
+					break;
+			}
+		}
+	}
 }
-
--- ReliveThePast/Plugin.cs
+++ ReliveThePast/Plugin.cs
@@ -3,72 +3,71 @@ using EXILED;
 
 namespace ReliveThePast
 {
-    public class Plugin : EXILED.Plugin
-    {
-        public bool EnableRelive;
-        public static double ReliveRespawnTimer;
-        public override string getName { get; } = "Re-live The Past - By DefyTheRush";
+	public class Plugin : EXILED.Plugin
+	{
+		public bool EnableRelive;
+		public static double ReliveRespawnTimer;
+		public override string getName { get; } = "Re-live The Past - By DefyTheRush; Modified by LambdaGaming";
 
-        public EventHandlers Handler;
+		public EventHandlers Handler;
 
-        public void ReloadConfig()
-        {
-            EnableRelive = Config.GetBool("relive_enable", true);
-            if (!EnableRelive)
-                Log.Info("Plugin disabled!");
-            else
-            {
-                Log.Info("Plugin enabled!");
-                CheckRespawnValue();
-            }
-        }
+		public void ReloadConfig()
+		{
+			EnableRelive = Config.GetBool( "relive_enable", true );
+			if ( !EnableRelive )
+				Log.Info( "Plugin disabled!" );
+			else
+			{
+				Log.Info( "Plugin enabled!" );
+				CheckRespawnValue();
+			}
+		}
 
-        public void CheckRespawnValue()
-        {
-            try
-            {
-                ReliveRespawnTimer = Config.GetDouble("relive_respawn_timer");
-            }
-            catch (Exception)
-            {
-                Log.Info("Detected invalid value in the configuration file! Using default value of 0.05");
-            }
-            finally
-            {
-                if (ReliveRespawnTimer < 0.05)
-                {
-                    ReliveRespawnTimer = 0.05;
-                    Config.SetString("relive_respawn_timer", ReliveRespawnTimer.ToString());
-                    Log.Info("ReliveThePast cannot use a value below 0.05 for respawning! Using default value of: " + ReliveRespawnTimer);
-                }
-            }
-        }
+		public void CheckRespawnValue()
+		{
+			try
+			{
+				ReliveRespawnTimer = Config.GetDouble( "relive_respawn_timer" );
+			}
+			catch ( Exception )
+			{
+				Log.Info( "Detected invalid value in the configuration file! Using default value of 0.05" );
+			}
+			finally
+			{
+				if ( ReliveRespawnTimer < 0.05 )
+				{
+					ReliveRespawnTimer = 0.05;
+					Config.SetString( "relive_respawn_timer", ReliveRespawnTimer.ToString() );
+					Log.Info( "ReliveThePast cannot use a value below 0.05 for respawning! Using default value of: " + ReliveRespawnTimer );
+				}
+			}
+		}
 
-        public override void OnDisable()
-        {
-            Events.RemoteAdminCommandEvent -= Handler.RunOnCommand;
-            Events.PlayerDeathEvent -= Handler.RunOnPlayerDeath;
-            Events.RoundRestartEvent -= Handler.RunOnRoundRestart;
-            Handler = null;
-        }
+		public override void OnDisable()
+		{
+			Events.RemoteAdminCommandEvent -= Handler.RunOnCommand;
+			Events.PlayerDeathEvent -= Handler.RunOnPlayerDeath;
+			Events.RoundRestartEvent -= Handler.RunOnRoundRestart;
+			Handler = null;
+		}
 
-        public override void OnEnable()
-        {
-            ReloadConfig();
-            if (!EnableRelive)
-                return;
+		public override void OnEnable()
+		{
+			ReloadConfig();
+			if ( !EnableRelive )
+				return;
 
-            Log.Info("Starting up \"Re-live The Past - By DefyTheRush\"");
-            Handler = new EventHandlers();
-            Events.RoundRestartEvent += Handler.RunOnRoundRestart;
-            Events.PlayerDeathEvent += Handler.RunOnPlayerDeath;
-            Events.RemoteAdminCommandEvent += Handler.RunOnCommand;
-        }
+			Log.Info( "Starting up \"Re-live The Past - By DefyTheRush; Modified by LambdaGaming\"" );
+			Handler = new EventHandlers();
+			Events.RoundRestartEvent += Handler.RunOnRoundRestart;
+			Events.PlayerDeathEvent += Handler.RunOnPlayerDeath;
+			Events.RemoteAdminCommandEvent += Handler.RunOnCommand;
+		}
 
-        public override void OnReload()
-        {
-            Log.Info("Reloading \"Re-live The Past - By DefyTheRush\"!");
-        }
-    }
+		public override void OnReload()
+		{
+			Log.Info( "Reloading \"Re-live The Past - By DefyTheRush; Modified by LambdaGaming\"!" );
+		}
+	}
 }
-

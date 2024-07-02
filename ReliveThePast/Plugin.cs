f5d00fb41289038feac76596694ab5ecdab0a7c0
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-07-02 12:58:12 -0400

    Add author and remove extra load messages

--- ReliveThePast/Plugin.cs
+++ ReliveThePast/Plugin.cs
@@ -8,19 +8,18 @@ namespace ReliveThePast
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 2, 4, 2 );
+		public override Version Version { get; } = new Version( 2, 4, 3 );
 		public override Version RequiredExiledVersion { get; } = new Version( 8, 0, 0 );
+		public override string Author { get; } = "OPGman";
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
 		public override void OnEnabled()
 		{
 			base.OnEnabled();
-			Log.Info( "Starting up \"Re-live The Past - By DefyTheRush; Modified by LambdaGaming\"" );
 			EventHandlers = new EventHandlers( this );
 			events.Player.Died += EventHandlers.RunOnPlayerDeath;
 			events.Server.RoundStarted += EventHandlers.OnRoundStart;
 			events.Server.RoundEnded += EventHandlers.OnRoundEnd;
-			Log.Info( $"Successfully loaded." );
 		}
 
 		public override void OnDisabled()
